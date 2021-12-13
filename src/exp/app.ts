2b9a72337be3aabae99b1475bf1fcc6e1a34c1ae
liufeihe <lfhe2014@163.com>
2021-12-13 18:44:25 +0800

    fix

--- src/exp/app.ts
+++ src/exp/app.ts
@@ -1,3 +1,4 @@
+
 function getExpStrList(expression: string) {
   expression = expression.replace(new RegExp('[(]', 'g'), '');
   expression = expression.replace(new RegExp('[)]', 'g'), '');
@@ -22,10 +23,47 @@ function getVarFromExp(exp: string) {
   return varList.map(item => item.trim());
 }
 
-function main() {
-  const expression = '(a < 1)&(b >1 |(c>2|d<a))';
-  const expStrList = getExpStrList(expression);
-  console.log(expStrList);
+function parseExp(expression: string) {
+  const logicOps = new Set(['&', '|']);
+  let left: string, op: string, right:string;
+  let logicIdx: number;
+  let parenthesisCnt = 0;
+  for (let i=0; i<expression.length; i++) {
+    const ch = expression[i];
+    if (ch === '(') {
+      parenthesisCnt++;
+    } else if (ch === ')') {
+      parenthesisCnt--;
+    }
+    if (parenthesisCnt===0 && logicOps.has(ch)) {
+      logicIdx = i;
+      break;
+    }
+  }
+  if (logicIdx===undefined) {
+    if (expression[0]==='(' && expression[expression.length-1]===')') {
+      parseExp(expression.substring(1,expression.length-1))
+    }
+  } else {
+    left = expression.substring(0,logicIdx);
+    op = expression.substring(logicIdx, logicIdx+1);
+    right = expression.substring(logicIdx+1);
+  }
+  console.log({left, op, right});
+  if (left && (left.includes('&') || left.includes('|'))) {
+    parseExp(left);
+  }
+  if (right && (right.includes('&') || right.includes('|'))) {
+    parseExp(right);
+  }
 }
+
+function main() {
+  const expression = '(((a<1)&(b>1|(c>2|d<a)))&(d<1))';
+  // const expStrList = getExpStrList(expression);
+  // console.log(expStrList);
+  parseExp(expression);
+}
+
 
 main();
