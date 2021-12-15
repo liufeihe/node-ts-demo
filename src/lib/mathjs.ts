92dfa483fcbef4cc79303590a08ee5f631b07493
liufeihe <lfhe2014@163.com>
2021-12-16 01:53:34 +0800

    fix

--- src/lib/mathjs.ts
+++ src/lib/mathjs.ts
@@ -1,6 +1,31 @@
-import { evaluate } from 'mathjs';
+import { evaluate, parse } from 'mathjs';
 
-function main() {
+function doParse(expression: string) {
+  const node = parse(expression);
+  const facts = {volume:100, volume5:20, userTag:1};
+  console.log(expression, node);
+  checkNode(node, facts);
+}
+
+function checkNode(node: math.MathNode, facts: any) {
+  if (!['and', 'or', '==', '!=', '>', '>=', '<', '<=', '+', '-', '*', '/'].includes(node.op)) {
+    console.log('op is invalid', node.op);
+    return;
+  }
+  if (['and', 'or'].includes(node.op)) {
+    // 检查args
+    node.args.forEach(node => {
+      checkNode(node, facts);
+    })
+  } else {
+    const value = node.evaluate(facts);
+    if (value!==false && value!==true) {
+      console.log('node不是一个表达式', node);
+    }
+  }
+}
+
+function doEvaluate() {
   try {
     // const exps = ['a==1e2', 'b>1e1','a>b']
     // const res = evaluate(exps, {a:100, b:1.1e1})
@@ -25,12 +50,17 @@ function main() {
     // console.log(evaluate('1 | 0 | 1'));
     // console.log(evaluate('true | false | true'));
 
-    console.log(evaluate('a/b > 1000', {a: 10, b: 0}));
-    console.log(evaluate('a/b', {a: 10, b: 0}));
+    // console.log(evaluate('a/b > 1000', {a: 10, b: 0}));
+    // console.log(evaluate('a/b', {a: 10, b: 0}));
+    console.log(evaluate('volume+2>200&volume5*3>userTag+5|userTag==1', {volume:100, volume5:20, userTag:1}))
   } catch (error) {
     console.log(error);
   }
-  
+}
+
+function main() {
+  doEvaluate();
+  doParse('volume+2 & volume5*3>userTag+5 or userTag<=10');
 }
 
 main()
