0c5a2026648347dfccf0a3b360fed099be263146
liufeihe <lfhe2014@163.com>
2022-01-06 02:30:29 +0800

    fix

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,3 +1,6 @@
+import { isObject } from "lodash";
+
+
 function camelToUnderline(key: string) {
   let key2 = key.replace(/([A-Z])/g, (s) => {
     return ' '+s.toLowerCase();
@@ -8,7 +11,11 @@ function camelToUnderline(key: string) {
 function transferDatakeyToUnderLine(data: Record<string, any>): Record<string, any> {
   const data2: Record<string, any> = {};
   Object.keys(data).forEach(key => {
-    data2[camelToUnderline(key)] = data[key];
+    let value = data[key];
+    if (isObject(value)) {
+      value = transferDatakeyToUnderLine(value);
+    }
+    data2[camelToUnderline(key)] = value;
   })
   return data2;
 }
